7c2e6064a60775a105da28d35ed81d6c8701e70d
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-01-18 15:02:36 +0000

    Helpers - Challenge 21.

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 
-feature "Enter names" do
-  scenario "submitting names" do
-    sign_in_and_play
-    expect(page).to have_content "Dylan vs. Ben"
-  end
-end
-
 feature "View HP" do
   scenario "viewing opponent's HP" do
     sign_in_and_play
