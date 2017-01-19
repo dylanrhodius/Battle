0574aa89649906869d912367dc9b512a037c7bed
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-01-19 19:44:14 +0000

    Finish first user story CH28

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 feature "View HP" do
-  scenario "viewing opponent's HP" do
+  scenario "viewing Player 2 HP" do
     sign_in_and_play
-    expect(page).to have_content ": 60HP"
+    expect(page).to have_content "Ben: 60HP"
+  end
+  scenario "viewing Player 1 HP" do
+    sign_in_and_play
+    expect(page).to have_content "Dylan: 60HP"
   end
 end
