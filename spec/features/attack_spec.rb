c7be858587b3bb9d8a295f46b32ecc80789a96e6
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-01-19 20:13:26 +0000

    2nd User story CH28. Changed links for buttons.

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,28 +3,35 @@ require 'spec_helper'
 feature "Attack" do
   scenario "attacking player 2" do
     sign_in_and_play
-    click_link "Attack"
+    click_button "Attack"
     expect(page).to have_content "Dylan attacked Ben"
+  end
+  scenario "attacking player 1" do
+    sign_in_and_play
+    click_button "Attack"
+    click_button "OK"
+    click_button "Attack"
+    expect(page).to have_content "Ben attacked Dylan"
   end
 end
 
 feature "Attack reduces player 2's HP." do
   scenario "Player 1 is attacking player 2" do
     sign_in_and_play
-    click_link "Attack"
-    click_link "OK"
+    click_button "Attack"
+    click_button "OK"
     expect(page).not_to have_content "Ben: 60HP"
     expect(page).to have_content "Ben: 50HP"
   end
 end
 
-feature "Switching player turns." do
-  scenario "Player 2 attacking player 1." do
-    sign_in_and_play
-    click_link "Attack"
-    click_link "OK"
-    expect(page).not_to have_content "Ben: 60HP"
-    expect(page).to have_content "Ben: 50HP"
-    expect(page).to have_content "It's Ben's turn to attack."
-  end
-end
+# feature "Switching player turns." do
+#   scenario "Player 2 attacking player 1." do
+#     sign_in_and_play
+#     click_link "Attack"
+#     click_link "OK"
+#     expect(page).not_to have_content "Ben: 60HP"
+#     expect(page).to have_content "Ben: 50HP"
+#     expect(page).to have_content "It's Ben's turn to attack."
+#   end
+# end
